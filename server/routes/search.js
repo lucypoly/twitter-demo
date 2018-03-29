cde5ef4b2f8923e2448589dfb87a618911e3d9a1
Liudmyla_Polianychko <Liudmula_Polianychko@epam.com>
2018-03-29 12:17:34 +0300

    refactoring

--- server/routes/search.js
+++ server/routes/search.js
@@ -3,42 +3,6 @@ const router = express.Router();
 const oauth = require('../oauth');
 const constants = require('../constants');
 
-// /**
-//  * Retrieve the identified Collection, presented as a list of the Tweets curated within
-//  * @id - The identifier of the Collection for which to return results
-//  * **/
-// router.get('/:id', (req, res) => {
-//   oauth.get(
-//     constants.BASE + 'collections/entries.json?id=' + req.params.id, constants.TOKEN, constants.SECRET,
-//     function (e, data, response) {
-//       res.send(data);
-//     });
-// });
-//
-// /**
-//  * Create a Collection owned by the currently authenticated user
-//  * @name - The title of the collection being created, in 25 characters or less
-//  * **/
-// router.post('/:id', (req, res) => {
-//   oauth.post(
-//     constants.BASE + 'collections/create.json?name=' + req.params.id, constants.TOKEN, constants.SECRET,
-//     function (e, data, response) {
-//       res.send(data);
-//     });
-// });
-//
-// /**
-//  * Curate a Collection by adding Tweets in bulk
-//  * **/
-// router.post('/', (req, res) => {
-//   const params = { id: 'custom-978631851660234753', changes: [{ op: 'add', tweet_id: '390853164611555329' }] };
-//
-//   oauth.post(
-//     constants.BASE + 'collections/entries/curate.json', constants.TOKEN, constants.SECRET,
-//     JSON.stringify(params), 'application/json', function (e, data, response) {
-//       res.send(data);
-//     });
-// });
 /**
  * Retrieve the identified Collection, presented as a list of the Tweets curated within
  * @id - The identifier of the Collection for which to return results
@@ -47,7 +11,7 @@ const searchTweets = (id) => new Promise((resolve, reject) => {
   return oauth.get(
     constants.BASE + 'search/tweets.json?q=' + id + '&count=10', constants.TOKEN, constants.SECRET,
     (e, data, response) => {
-        resolve(JSON.parse(data));
+      resolve(JSON.parse(data));
     });
 });
 
@@ -59,7 +23,7 @@ const createTimeline = (name) => new Promise((resolve, reject) => {
   return oauth.post(
     constants.BASE + 'collections/create.json?name=' + name, constants.TOKEN, constants.SECRET,
     (e, data, response) => {
-        resolve(JSON.parse(data));
+      resolve(JSON.parse(data));
     });
 });
 
@@ -86,11 +50,11 @@ getTimeline = async (id) => {
   let collection = await createTimeline(id);
   console.log(collection);
 
-  tweets = collection.statuses.map(item => item.id_str);
-  params = { id: timeline.id };
   timeline.id = collection.response.timeline_id;
   console.log(timeline);
 
+  params = { id: timeline.id };
+  tweets = collection.statuses.map(item => item.id_str);
   params.changes = tweets.map(tweet => {
     return { op: 'add', tweet_id: tweet };
   });
@@ -102,35 +66,9 @@ getTimeline = async (id) => {
 
 
 router.get('/:id', (req, res) => {
-  // oauth.get(
-  //   constants.BASE + 'search/tweets.json?q=' + req.params.id + '&count=10', constants.TOKEN, constants.SECRET,
-  //   (e, data, response) => {
-  //
-  //     //save id for next results
-  //     const timeline = {};
-  //     timeline.next_results = JSON.parse(data).search_metadata.next_results;
-  //
-  //     //create new timeline
-  //     oauth.post(
-  //       constants.BASE + 'collections/create.json?name=' + req.params.id, constants.TOKEN, constants.SECRET,
-  //       (e, data, response) => {
-  //         console.log(data);
-  //         timeline.id = JSON.parse(data).response.timeline_id;
-  //       });
-  //
-  //     //add tweets to timeline
-  //     const tweets = JSON.parse(data).statuses.map(item => item.id_str);
-  //     const params = { id: timeline.id };
-  //     params.changes = tweets.map(tweet => {
-  //       return { op: 'add', tweet_id: tweet };
-  //     });
-  //
-  //     oauth.post(
-  //       constants.BASE + 'collections/entries/curate.json', constants.TOKEN, constants.SECRET,
-  //       JSON.stringify(params), 'application/json', function (e, data, response) {
-  //       });
-
   const timeline = getTimeline(req.params.id);
+  console.log(timeline);
+
   res.send(timeline);
 });
 
